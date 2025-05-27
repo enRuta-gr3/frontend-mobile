4ec6ddc45695612c0ef70ab0f734d5bd170a2e06
bfariello <bfariellosierra@gmail.com>
2025-05-26 23:19:01 -0300

    se eliminan

--- controllers/ticketHistory.ts
+++
@@ -1,9 +0,0 @@
-import viajes from '../json/viajes.json';
-
-export function getTicketHistory() {
-  return viajes.map(v => ({
-    ...v,
-    estado: 'Viajado', 
-    precio: `$${v.precio}`
-  }));
-}
