aaf64979cfb13fc9fcf837ab5cca512fc0e18542
bfariello <bfariellosierra@gmail.com>
2025-06-12 21:51:41 -0300

    test

--- controllers/apiVentas.tsx
+++ controllers/apiVentas.tsx
@@ -1,3 +1,5 @@
+
+
 export default async function capturarOrder(idVenta: number, token: string) {
   try {
     const res = await fetch('https://backend-production-2812f.up.railway.app/api/venta/confirmarVentaPaypal', {
